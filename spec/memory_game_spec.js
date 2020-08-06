7d19286a3ab9389a83d75349fc836ce47d3200a5
lednah88 <lednah97@gmail.com>
2020-08-06 11:09:30 +0200

    spec file updated

--- spec/memory_game_spec.js
+++ spec/memory_game_spec.js
@@ -1,1 +1,32 @@
-const {startGame, flipper} = require("../src/memory_game")
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+describe("index.html", () => {
+  var browser;
+  beforeEach(() => {
+    JSDOM.fromFile("../src/index.html", {
+
+    }).then((res) => {
+      browser = res;
+      done();
+    });
+  });
+  afterEach(() => {
+    browser.window.close();
+  });
+  it("should have a h1 elemet", () => {
+    var h1 = browser.window.document.querySelector("h1");
+    expect(h1).not.toBeNull();
+  });
+  it("should have a div elemet", () => {
+    var div = browser.window.document.querySelector("div");
+    expect(div).not.toBeNull();
+  });
+  it("should have a script elemet", () => {
+    var script = browser.window.document.querySelector("script");
+    expect(script).not.toBeNull();
+  });
+  it("should have an array of numbers",()=>{
+    JSDOM.fromFile("../src/memory_game.js")
+    expect(numbers).toBeDefined()
+  })
+});
--- src/memory_game.js
+++ src/memory_game.js
@@ -1,48 +1,55 @@
-let numbers = ["1","1","2","2","3","3","4","4","5","5","6","6"]
+let numbers = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6"];
 let pair = [];
 let cardIds = [];
 let flippedCards = 0;
-    
- function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let newPosition = Math.floor(Math.random() * (i + 1));
-        [array[i], array[newPosition]] = [array[newPosition], array[i]];
-    }
-    return array;
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let newPosition = Math.floor(Math.random() * (i + 1));
+    [array[i], array[newPosition]] = [array[newPosition], array[i]];
+  }
+  return array;
 }
-    
-    function startGame() {
-        flippedCards = 0;
-        let cardDiv = '';
-        shuffle(numbers);
-        for (let i = 0; i < numbers.length; i++)
-            cardDiv += '<div id="card_'+ i +'" onclick="flipper(this,\'' + numbers[i] + '\')"></div>';
-        document.getElementById('board').innerHTML = cardDiv;
+
+function startGame() {
+  flippedCards = 0;
+  let cardDiv = "";
+  shuffle(numbers);
+  for (let i = 0; i < numbers.length; i++)
+    cardDiv +=
+      '<div id="card_' +
+      i +
+      '" onclick="flipper(this,\'' +
+      numbers[i] +
+      "')\"></div>";
+  document.getElementById("board").innerHTML = cardDiv;
+}
+
+function flipCard() {
+  var card1 = document.getElementById(cardIds[0]);
+  var card2 = document.getElementById(cardIds[1]);
+  card1.style.background = card2.style.background = "red";
+  card1.innerHTML = card2.innerHTML = "";
+  [pair, cardIds] = [[], []];
+}
+
+function flipper(card, number) {
+  if (card.innerHTML == "" && pair.length < 2) {
+    card.style.background = "blue";
+    card.innerHTML = number;
+
+    if (!pair[0] || !pair[1]) {
+      pair.push(number);
+      cardIds.push(card.id);
     }
-    
-    function flipCard(){
-        var card1 = document.getElementById(cardIds[0]);
-        var card2 = document.getElementById(cardIds[1]);
-        card1.style.background = card2.style.background = 'red';
-        card1.innerHTML = card2.innerHTML = "";
-        [pair, cardIds] = [[], []];
-    }
-    
-    function flipper(card, number) {
-        if (card.innerHTML == "" && pair.length < 2) {
-            card.style.background = 'blue';
-            card.innerHTML = number;
-    
-            if (!pair[0] || !pair[1]) {
-                pair.push(number);
-                cardIds.push(card.id);
-            }
-            
-            if (pair[0] && pair[1])
-                pair[0] == pair[1] ? (flippedCards += 2, [pair, cardIds] = [[], []]) : setTimeout(flipCard, 1100);
-        }
-    
-        if (flippedCards == numbers.length)
-            document.getElementById('board').innerHTML = '<h1>🥇<u>Well done! you won</u>🥇</h1></br><button onclick="startGame()">Start New Game...</button>';
-    }
-module.exports ={startGame, flipper}
+
+    if (pair[0] && pair[1])
+      pair[0] == pair[1]
+        ? ((flippedCards += 2), ([pair, cardIds] = [[], []]))
+        : setTimeout(flipCard, 1100);
+  }
+
+  if (flippedCards == numbers.length)
+    document.getElementById("board").innerHTML =
+      '<h1>🥇<u>Well done! you won</u>🥇</h1></br><button onclick="startGame()">Start New Game...</button>';
+}
